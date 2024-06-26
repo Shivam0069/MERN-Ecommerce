9810c9c07d1e6d4ac8cab0f141d835ef8c013647
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2024-06-26 18:52:49 +0530

    Added Types for different Variables

---
+++ types/reducer-types.ts
@@ -0,0 +1,34 @@
+import { CartItem, ShippingInfo, User } from "./types";
+
+export interface UserReducerInitialState {
+  user: User | null;
+  loading: boolean;
+}
+
+export interface CartReducerInitialState {
+  loading: boolean;
+
+  cartItems: CartItem[];
+  subtotal: number;
+  tax: number;
+  shippingCharges: number;
+  discount: number;
+  total: number;
+  shippingInfo: ShippingInfo;
+  message: string;
+  error: string | null;
+}
+
+export type CartSchema = {
+  user: string;
+  cartItems: CartItem[];
+  subtotal: number;
+  tax: number;
+  shippingCharges: number;
+  discount: number;
+  total: number;
+  shippingInfo: ShippingInfo;
+};
+export type NewCartRequest = {
+  formData: CartSchema;
+};
