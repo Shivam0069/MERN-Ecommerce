703d958eab19a3fb09717fb627d9fc569aad9901
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2024-06-28 00:24:25 +0530

    Home Page Device Responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
 "use client";
 import banner from "@/assets/banner.jpg";
 import banner2 from "@/assets/banner2.jpg";
+import banner3 from "@/assets/banner3.jpg";
 import ProductCard from "@/components/productCard";
 import Loader, { CircleLoader } from "@/helper/loader";
 import { useLatestProductsQuery } from "@/store/api/productAPI";
-import { addToCart, calculatePrice } from "@/store/slice/cartSlice";
+import { addToCart } from "@/store/slice/cartSlice";
+import { UserState } from "@/store/slice/userSlice";
 import { RootState } from "@/store/store";
 import { CartItem } from "@/types/types";
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function Home() {
   const { data, isLoading, isError } = useLatestProductsQuery("");
+  const user = useSelector((state: RootState) => state.user.userData);
   const [loading, setLoading] = useState<boolean>(false);
   const { cartItems } = useSelector((state: RootState) => state.cart);
   const dispatch = useDispatch();
@@ -32,15 +35,16 @@ export default function Home() {
       setLoading(false);
       return toast.error("Out of stock!");
     }
-    dispatch(addToCart(cartItem));
-    dispatch(calculatePrice());
+
+    dispatch(addToCart({ cartItem, user: user?._id }));
+
     toast.success("Product added to cart");
     setLoading(false);
   };
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % 2);
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % 3);
     }, 5000);
     return () => clearInterval(interval);
   }, []);
@@ -50,18 +54,25 @@ export default function Home() {
   }
 
   return (
-    <div className="max-w-7xl mx-auto pt-4 ">
+    <div className="max-w-7xl mx-auto pt-4">
       {loading && <Loader />}
-      <section className="overflow-hidden flex items-center relative ">
+      <section className="overflow-hidden mx-1 sm:mx-2 md:mx-3 flex items-center relative">
         <div
-          className="flex transition-transform ease-in-out duration-1000"
+          className="flex transition-transform ease-in-out duration-1000 "
           style={{ transform: `translateX(-${currentImageIndex * 100}%)` }}
         >
-          <img src={banner.src} className="w-full flex-shrink-0" />
-          <img src={banner2.src} className="w-full flex-shrink-0" />
+          <img src={banner.src} className="w-full flex-shrink-0 h-96" />
+          <img
+            src={banner2.src}
+            className="w-full flex-shrink-0 h-96 object-cover"
+          />
+          <img
+            src={banner3.src}
+            className="w-full flex-shrink-0 h-96 object-cover"
+          />
         </div>
       </section>
-      <div className="max-w-6xl mx-auto flex justify-between items-center pt-10">
+      <div className="max-w-6xl mx-1 sm:mx-2 md:mx-3 0 lg:mx-auto flex justify-between items-center pt-10 px-4 md:px-0">
         <div className="text-xl font-medium">Latest Products</div>
         <Link href="/search" className="text-sm">
           More
@@ -69,11 +80,11 @@ export default function Home() {
       </div>
 
       {isLoading ? (
-        <div className="w-screen mx-auto">
+        <div className="w-full flex justify-center items-center py-10">
           <CircleLoader />
         </div>
       ) : (
-        <div className="py-10 max-w-6xl mx-auto grid justify-items-center  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <div className="py-10 max-w-6xl mx-1 sm:mx-2 md:mx-3 lg:mx-auto grid justify-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-4 md:px-0">
           {data?.products.map((i) => (
             <ProductCard
               key={i._id}
