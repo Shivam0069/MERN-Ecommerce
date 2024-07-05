04b04e2854d95eecbc1ff5d558a9ba57cbe66ea5
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2024-07-05 23:28:14 +0530

    Error of Upload Icon

--- app/admin/transaction/[id]/page.tsx
+++ app/admin/transaction/[id]/page.tsx
@@ -32,7 +32,6 @@ import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { useSelector } from "react-redux";
 import "../../../globals.css";
-import { UploadIcon } from "../../product/[id]/page";
 const TransactionManagement = ({ params }: { params: { id: string } }) => {
   const { id } = params;
   const [order, setOrder] = useState<Order>();
@@ -229,6 +228,27 @@ const TransactionManagement = ({ params }: { params: { id: string } }) => {
 };
 
 export default TransactionManagement;
+
+function UploadIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+      <polyline points="17 8 12 3 7 8" />
+      <line x1="12" x2="12" y1="3" y2="15" />
+    </svg>
+  );
+}
 
 function DotIcon(props: any) {
   return (
