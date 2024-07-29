762f871814551518a0828a8a0e1209345c57d59f
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2024-07-30 01:24:41 +0530

    reset password email content

--- app/api/forgot-password/route.ts
+++ app/api/forgot-password/route.ts
@@ -48,7 +48,8 @@ export async function POST(request: NextRequest) {
         <p>Dear User,</p>
         <p>We received a request to reset your password for your Flash Buy account. Please click the link below to reset your password:</p>
         <p><a href="https://flash-buy.vercel.app/resetpassword/${user._id}?token=${token}">Reset Password</a></p>
-        <p>If you did not request this password reset, please ignore this email or contact support if you have questions.</p>
+        <p><strong>Important:</strong> This link will expire in 10 minutes. If you do not reset your password within this time frame, you will need to request a new password reset link.</p>
+        <p>If you did not request this password reset, please ignore this email or contact support if you have any questions.</p>
         <p>Thank you,</p>
         <p>The Flash Buy Team</p>
       </div>
